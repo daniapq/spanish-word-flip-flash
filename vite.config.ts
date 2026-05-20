1eb8a57a608e301d6584c2e6dbf181409606f2bc
daniapq <daniel.aparicio.q@gmail.com>
2026-05-19 20:27:59 -0400

    Updating vite

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ],
+  build: {
+    target: 'es2022' // Or ['chrome87', 'edge88', 'es2022', 'firefox78', 'safari14.1']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
